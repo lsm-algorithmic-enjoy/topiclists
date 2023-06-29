cbd54c81bf21ff29c117b781ab062a209c63d0ad
seungmin-lee-dotcom <k07217@naver.com>
2023-06-29 23:26:48 +0900

    Feat: get_topics

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 #토픽 리스트 보여주기
 
+프로젝트 진행 순서 정리
+
 1. CRA (TypeScript) 프로젝트 생성
 2. REST API 서버 구축을 위한 json-server 설치 및 topic.json 파일을 루트 디렉토리에 생성
    > npm install -g json-server
--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,36 @@
-import React from "react";
-import "./App.css";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+type Topic = {
+  idx: number;
+  title: string;
+  imgPath: string;
+};
 
 function App() {
-  return <div className="App"></div>;
+  const [topics, setTopics] = useState<Topic[]>([]);
+  const get_topics = async () => {
+    try {
+      const res = await axios.get<Topic[]>("http://localhost:4000/topics");
+      setTopics(res.data);
+    } catch (err) {}
+  };
+
+  useEffect(() => {
+    get_topics();
+  }, []);
+
+  return (
+    <ul>
+      {topics.map((topic) => (
+        <li key={topic.idx}>
+          <img src={topic.imgPath} />
+          <p>{topic.title}</p>
+        </li>
+      ))}
+      ;
+    </ul>
+  );
 }
 
 export default App;
