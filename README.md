3a7fa67af20fa8c65c82268cb3beef70780215cc
seungmin-lee-dotcom <k07217@naver.com>
2023-07-02 19:43:03 +0900

    Feat: isLiked

--- README.md
+++ README.md
@@ -6,10 +6,11 @@
 2. REST API 서버 구축을 위한 json-server 설치 및 topic.json 파일을 루트 디렉토리에 생성
    > npm install -g json-server
    > json-server --version
-3. topic.json 기반 로컬 서버 실행 (4000번 포트 지정), http://localhost:4000/topics 경로에 로컬 서버 생성
-   > json-server -p 4000 topic.json
+3. topic.json 기반 로컬 서버 실행 (4000번 포트 지정), http://localhost:4000/topics 경로에 로컬 서버 생성, idx를 통한 데이터 업데이트를 위해 routes.json 파일에서 엔드포인트 설정
+   > json-server --watch topic.json --routes routes.json --id idx --port 4000
 4. API 요청을 위한 axios 설치
    > npm install axios
 5. axios를 이용하여 HTTP 메소드 중 하나인 get으로 토픽 리스트를 가져와 보여주도록 구현
 6. 정규표현식을 이용한 Topic title 검색 기능 구현
 7. filter 메서드를 사용하여 주어진 토픽 리스트를 grade 카테고리 별로 필터링하는 기능 구현
+8. 좋아요 상태 업데이트 기능 구현(HTTP 메소드 중 하나인 patch를 통해 isLiked 데이터 업데이트, 새로고침 후에도 좋아요 상태 유지)
--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,20 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 type Topic = {
-  idx: number;
+  idx: string;
   title: string;
   imgPath: string;
   grade: string;
+  isLiked: boolean;
 };
 
 type Props = {
   topics: Topic[];
   topicPrefix: string;
+  setTopics: React.Dispatch<React.SetStateAction<Topic[]>>;
 };
 
-function TopicMessage({ topics, topicPrefix }: Props) {
+function TopicMessage({ topics, topicPrefix, setTopics }: Props) {
   //topicprefix와 토픽 타이틀이 일치하는 토픽 리스트를 보여주기, 빈 문자열인 경우 모든 토픽 리스트 디폴트로 출력
   const escapetopic = topicPrefix.replace(/[.*+?^${}()|[\]\\]/g, "\\$&"); // 특수 문자 이스케이프 처리
   const filteredTopics = topics.filter((topic) => {
@@ -21,13 +23,36 @@ function TopicMessage({ topics, topicPrefix }: Props) {
     return topic.title.match(regex);
   });
 
+  async function toggleLike(idx: string) {
+    const topicToUpdate = topics.find((topic) => topic.idx === idx);
+    if (!topicToUpdate) return;
+
+    const updatedTopic = {
+      ...topicToUpdate,
+      isLiked: !topicToUpdate.isLiked,
+    };
+
+    await axios.patch(`http://localhost:4000/topics/${idx}`, {
+      isLiked: updatedTopic.isLiked,
+    });
+
+    const updatedTopics = topics.map((topic) =>
+      topic.idx === idx ? updatedTopic : topic
+    );
+
+    setTopics(updatedTopics);
+  }
+
   return (
     <ul>
-      {filteredTopics.map(({ title, imgPath, idx }) => {
+      {filteredTopics.map(({ title, imgPath, idx, isLiked }) => {
         return (
           <li key={idx}>
             <img src={imgPath} alt={title} />
             <p>{title}</p>
+            <button onClick={() => toggleLike(idx)}>
+              {isLiked ? "지금 좋아" : "취소됨"}
+            </button>
           </li>
         );
       })}
@@ -90,6 +115,7 @@ function App() {
       <TopicMessage
         topics={getFilteredTopicsByGrade()}
         topicPrefix={topicPrefix}
+        setTopics={setTopics}
       />
     </>
   );
